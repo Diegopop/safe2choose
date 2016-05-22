cdd650c3fa2e404923834a983be40d67bd369965
Diego Hernández <goo.artpop@gmail.com>
2016-05-21 21:16:02 -0500

    add Diego code  refactored

--- chat.js
+++ chat.js
@@ -3,7 +3,7 @@
   var locale = url.split("/")[3];
   var onlineGreeting, offlineGreeting;
 
-  if(locale === "pl") {
+  if(locale === "pl" || locale === "pt-pt" || locale === "hi" || locale === "fr") {
     window.$zopim||(function(d,s){var z=$zopim=function(c){
     z._.push(c)},$=z.s=
     d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
@@ -66,51 +66,6 @@
         'online': onlineGreeting,
         'offline': offlineGreeting
         });
-    });
-  } else if(locale === "pt-pt") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Chat with us";
-    offlineGreeting = 'Leave us a message';
-    $zopim(function() {
-	    $zopim.livechat.setGreetings({
-	    'online': onlineGreeting,
-	    'offline': offlineGreeting
-	    });
-    });
-  } else if(locale === "hi") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Chat with us";
-    offlineGreeting = 'Leave us a message';
-    $zopim(function() {
-	    $zopim.livechat.setGreetings({
-	    'online': onlineGreeting,
-	    'offline': offlineGreeting
-	    });
-    });
-  } else if(locale === "fr") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Chat with us";
-    offlineGreeting = 'Leave us a message';
-    $zopim(function() {
-	    $zopim.livechat.setGreetings({
-	    'online': onlineGreeting,
-	    'offline': offlineGreeting
-	    });
     });
   } else {
     window.$zopim||(function(d,s){var z=$zopim=function(c){
