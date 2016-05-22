518e16486c962569b388a87bbb2ff159d1f7e18e
Diego Hernández <goo.artpop@gmail.com>
2016-05-21 20:42:19 -0500

    Add Diego chat.js update

--- chat.js
+++ chat.js
@@ -35,7 +35,38 @@
 	    'offline': offlineGreeting
 	    });
     });
+} else if(locale === "it") {
+    window.$zopim||(function(d,s){var z=$zopim=function(c){
+    z._.push(c)},$=z.s=
+    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+    $.src='//v2.zopim.com/?';z.t=+new Date;$.
+    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
 
+    onlineGreeting = "Parlaner con noi";
+    offlineGreeting = 'Ci lascia un messaggio';
+    $zopim(function() {
+        $zopim.livechat.setGreetings({
+        'online': onlineGreeting,
+        'offline': offlineGreeting
+        });
+    });
+} else if(locale === "ja") {
+    window.$zopim||(function(d,s){var z=$zopim=function(c){
+    z._.push(c)},$=z.s=
+    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+    $.src='//v2.zopim.com/?';z.t=+new Date;$.
+    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+
+    onlineGreeting = "私たちに話します";
+    offlineGreeting = '伝言を残します';
+    $zopim(function() {
+        $zopim.livechat.setGreetings({
+        'online': onlineGreeting,
+        'offline': offlineGreeting
+        });
+    });
   } else if(locale === "pt-pt") {
     window.$zopim||(function(d,s){var z=$zopim=function(c){
     z._.push(c)},$=z.s=
